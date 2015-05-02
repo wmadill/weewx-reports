b82544b61d172655e130cf384178a23e2987c809
bill <bill@pixelspoke.com>
2015-05-02 16:41:59 -0700

    fix typo testing report_interval

--- bin/user/reports.py
+++ bin/user/reports.py
@@ -7,8 +7,8 @@ class Reports(StdReport):
 
     # Override launch_report_thread:
     def launch_report_thread(self, event):
-	# TODO force hourly alignment
-        # Launch only every "report_interval" seconds
-	report_interval = self.config_dict['StdArchive']['Reporst'].as_int('report_interval')
-        if not self.launch_time or (time.time() - self.launch_time >= report_interval):
-            StdReport.launch_report_thread(self,event)
+    # TODO force hourly alignment
+    # Launch only every "report_interval" seconds
+    report_interval = self.config_dict['StdArchive']['Reports'].as_int('report_interval')
+    if not self.launch_time or (time.time() - self.launch_time >= report_interval):
+        StdReport.launch_report_thread(self,event)
